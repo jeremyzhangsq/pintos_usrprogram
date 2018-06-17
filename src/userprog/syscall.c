e003c68e053c89c765214de931818f4441fa10c9
jeremyzhangsq <jeremyzhangsq@gmail.com>
2018-06-17 10:38:17 +0800

    pass 24

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,8 +1,11 @@
-#include "userprog/syscall.h"
+#include "../userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <user/syscall.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "../threads/thread.h"
+#include "../threads/interrupt.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -15,6 +18,32 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+//    printf("syscall!\n");
+  int *esp = f->esp;
+  switch(*esp){
+    case SYS_WRITE:{
+      write(*(esp+1),*(esp+2),*(esp+3));
+      break;
+    }
+    case SYS_EXIT:{
+      exit(*(esp+1));
+        break;
+    }
+  }
+
+}
+int
+write(int fd, const void *buffer, unsigned size){
+  switch (fd){
+    case 1:{
+      putbuf(buffer,size);
+      return size;
+    }
+  }
+}
+
+void
+exit(int status){
+  printf("%s: exit(%d)\n",thread_current()->name,status);
+  thread_exit();
 }
